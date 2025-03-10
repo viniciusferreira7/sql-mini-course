6f383ec1d598e79b7fc98cb18ab1e3cd03cfb6eb
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-03-10 08:58:20 -0300

    feat: create tables

---
+++ postgresql/create-table.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS brands, clients, order_itens, orders, products;
+
+CREATE TABLE clients (
+  id INT PRIMARY KEY,
+  name VARCHAR(20) NOT NULL,
+  last_name VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE products (
+  id INT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  description TEXT NULL,
+  price NUMERIC CHECK(price > 0) NOT NULL,
+  quantity SMALLINT DEFAULT 0 
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  client_id INT NOT NULL REFERENCES clients(id),
+  product_id INT NOT NULL,
+  quantity SMALLINT NOT NULL
+);
