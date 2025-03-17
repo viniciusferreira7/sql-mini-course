384f2136910f488f474b9fd0f5f7b2fcd40f9878
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-03-17 08:39:04 -0300

    feat: create selects

---
+++ postgresql/select.sql
@@ -0,0 +1,14 @@
+SELECT * FROM clients;
+
+SELECT name, last_name FROM clients;
+
+SELECT * FROM products;
+
+SELECT name FROM products;
+
+SELECT name, description FROM products;
+
+SELECT name,  price, description
+FROM products;
+
+SELECT * FROM orders;
