2cdb56f98884e696849ae245f12dd4ccc3d89ee8
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-04-07 08:32:34 -0300

    feat: add agregate function max, min, sum and avg

--- postgresql/aggregation-functions.sql
+++ postgresql/aggregation-functions.sql
@@ -13,3 +13,24 @@ SELECT COUNT(DISTINCT quantity) AS products FROM products;
 
 SELECT COUNT(*) FROM products
 WHERE price >= 10;
+
+-- MAX
+
+SELECT MAX(price) FROM products;
+SELECT MAX(quantity) FROM products;
+SELECT MAX(name) FROM products;
+
+-- MIN
+
+SELECT MIN(quantity) FROM products;
+
+-- SUM
+
+SELECT SUM(price) AS sum FROM products;
+SELECT (SUM(price) / COUNT(*)) AS sum FROM products;
+
+-- AVG
+
+SELECT AVG(price) FROM products;
+SELECT AVG(quantity) FROM products;
+
