e52324a8196225b9ac633f7bb5c529d98df57c4b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-05 08:47:08 -0300

    feat: add aritmetic operators

---
+++ postgresql/arithmetic-operators.sql
@@ -0,0 +1,22 @@
+SELECT 10 + 12 AS result;
+
+SELECT -10 + 12 AS result;
+
+SELECT 10 + 6 * 3 AS result;
+
+SELECT (10 + 6) * 3 AS result;
+
+SELECT 18 % 2 AS result;
+
+SELECT 10 ^ 2 AS result;
+
+SELECT 10 ^ 2 AS result;
+
+SELECT 6.02 * 10 ^ 2 AS result;
+
+SELECT |/ 20 AS result;
+
+SELECT ||/ 20 AS result;
+
+SELECT @ -20 AS result;
+
